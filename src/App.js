8be62355bac1cf683b1cf509046ca8beeadb626c
Neftali Forero <neftaiof@gmail.com>
2018-08-21 16:44:57 -0500

    Component for header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+// Importing MATERIAL UI components
+import Button from '@material-ui/core/Button';
+// Theme configuration
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import orange from '@material-ui/core/colors/orange';
+// Importing own components
+import HeaderDfdb from './lib/HeaderDfdb';
+
+const theme = createMuiTheme({
+    palette: {
+      primary: teal,
+      secondary: orange,
+    },
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <MuiThemeProvider theme={theme}>
+        <HeaderDfdb title="Descarga De Base de Datos" classes="{root:'', menuButton:'', flex:''}"></HeaderDfdb>
+      <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      </MuiThemeProvider>
       </div>
     );
   }
